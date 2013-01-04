92560f764b1c081539881b322989c882787de91a
FotoVerite <matt@novafabrica.com>
2013-01-04 12:48:32 -0500

    updates section to include position and associations

--- db/migrate/20130103043801_create_sections.rb
+++ db/migrate/20130103043801_create_sections.rb
@@ -3,6 +3,7 @@ class CreateSections < ActiveRecord::Migration
     create_table :sections do |t|
       t.references :document
       t.string :title
+      t.interger :position
       t.timestamps
     end
   end
